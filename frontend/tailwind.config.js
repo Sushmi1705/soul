bdfe73656fc34921c4cbd179fcebbd8f00dbfb49
Sushmi1705 <128606067+Sushmi1705@users.noreply.github.com>
2026-07-02 14:29:30 +0400

    fix: map tailwind font-sans & .font-sans utility to var(--font-sans) to enable dynamic font switching

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
   	extend: {
+  		fontFamily: {
+  			sans: ['var(--font-sans)', 'sans-serif'],
+  			serif: ['var(--font-serif)', 'serif'],
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
